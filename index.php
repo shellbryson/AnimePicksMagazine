e71cc979e22c0bf4cedf1f040b804be95272e988
Shell <shell@shellbryson.com>
2012-10-07 17:15:48 +0100

    Experimenting with front page layout of articles

--- index.php
+++ index.php
@@ -7,114 +7,74 @@
                     <div id="main" class="eightcol first clearfix" role="main">
                         
                         
-                        <section class="news lozenge">
-                            <h2>News</h2>
+                        <section class="updates lozenge">
+                            <h1>Updates</h1>
                             <ul>
                             <?php
                                 /* 
                                 ################################################
-                                NEWS
+                                UDPATES
                                 ################################################
                                 */ 
                                 
                                 // LOOP TRU THE POSTS, WE ONLY WANT TO DISPLAY THE LATEST
-                                $i = 1;
-                                $args = array( 'numberposts' => 20, 'category_name' => 'News' );
-                                $newsitems = get_posts( $args );
+                                $postCounter = 1;
+                                $args = array( 'numberposts' => 20, 'category_name' => 'News, Reviews' );
+                                $articles = get_posts( $args );
                                 $ns = "";
                         
-                                foreach( $newsitems as $news ) : setup_postdata( $news );
+                                foreach( $articles as $article ) : setup_postdata( $article );
                         
                                     // NEWS 411 FROM POST DATA
-                                    $id = $news          -> ID;
-                                    $title = $news       -> post_title;
-                                    $excerpt = $news     -> post_excerpt;
-                                    $date = $news        -> post_date;
+                                    $id = $article          -> ID;
+                                    $title = $article       -> post_title;
+                                    $excerpt = $article     -> post_excerpt;
+                                    $date = $article        -> post_date;
                                     
                                     // LOOKUP OTHER DATA
-                                    $link = $news        =  get_permalink( $id );
-                                    $image               =  get_the_post_thumbnail( $id, 'thumbnail' );
+                                    $link = $article        =  get_permalink( $id );
+                                    $image                  =  get_the_post_thumbnail( $id, 'thumbnail' );
+                                    $category               =  get_category(); // this is wrong, as posts may have many cats...?
                         
                                     // PROCESS DATE
                                     $dateformatted = date( 'dS M', strtotime( $date ) );
                                     
-                                    // DISPLAY HTML
-                                    ?>
-                        
-                                    <li class="news_item">
-                                        <article>
-                                            <section class="details">
-                                                <h3><a href="<?php echo $link;?>"><?php echo $title;?></a></h3>
-                                                <p><?php echo $excerpt;?></p>
-                                            </section>
-                                            <section class="image">
-                                                <a href="<?php echo $link;?>"><?php echo $image;?></a>
-                                                <div class="news_item_date"><?php echo $dateformatted;?></div>
-                                            </section>
-                                        </article>
-                                    </li>
-                                    <?php
-                                endforeach;
-                            ?>
-                            </ul>
-                            <div class="more"><a href="/category/news/">More news →</a></div>
-                        </section>
-                        <section class="reviews lozenge">
-                            <h2>Reviews</h2>
-                            <ul>
-                            <?php
-                                /* 
-                                ################################################
-                                REVIEWS
-                                ################################################
-                                */ 
-                                
-                                // LOOP TRU THE POSTS, WE ONLY WANT TO DISPLAY THE LATEST
-                                $i = 1;
-                                $args = array( 'numberposts' => 20, 'category_name' => 'News' );
-                                $newsitems = get_posts( $args );
-                                $ns = "";
-                        
-                                foreach( $newsitems as $news ) : setup_postdata( $news );
-                        
-                                    // NEWS 411 FROM POST DATA
-                                    $id = $news          -> ID;
-                                    $title = $news       -> post_title;
-                                    $excerpt = $news     -> post_excerpt;
-                                    $date = $news        -> post_date;
-                                    
-                                    // LOOKUP OTHER DATA
-                                    $link = $news        =  get_permalink( $id );
-                                    $image               =  get_the_post_thumbnail( $id, 'thumbnail' );
-                        
-                                    // PROCESS DATE
-                                    $dateformatted = date( 'dS M', strtotime( $date ) );
-                                    
-                                    // DISPLAY HTML
-                                    if ( $i < 4) {
-                                        $i++;
-                                    ?>
-                        
-                                    <li class="news_item">
-                                        <article>
-                                            <section class="details">
-                                                <h3><a href="<?php echo $link;?>"><?php echo $title;?></a></h3>
-                                                <p><?php echo $excerpt;?></p>
-                                            </section>
-                                            <section class="image">
-                                                <a href="<?php echo $link;?>"><?php echo $image;?></a>
-                                                <div class="news_item_date"><?php echo $dateformatted;?></div>
-                                            </section>
-                                        </article>
-                                    </li>
-                                    <?php
+                                    if ( $postCounter <= 8 ) {
+                                        $neko_box_class = "neko_box_large";
+                                    } else if ( $postCounter > 8 && $postCounter <= 14) {
+                                        $neko_box_class = "neko_box_medium";
+                                    } else if ( $postCounter > 14 ) {
+                                        $neko_box_class = "neko_box_small";
                                     }
+                                    ?>
+                        
+                                    <li>
+                                        <article class="neko_box <?php echo $neko_box_class?>">
+                                            <?php echo $image;?>
+                                            <header>
+                                                Anime Review
+                                                <?php echo $category;?>
+                                            </header>
+                                            <section class="details">
+                                                <h3><a href="<?php echo $link;?>"><?php echo $title;?></a></h3>
+                                                <p><?php echo $excerpt;?></p>
+                                            </section>
+                                            <section class="image">
+                                                <!--<a href="<?php echo $link;?>"><?php echo $image;?></a>-->
+                                                <!--<div class="news_item_date"><?php echo $dateformatted;?></div>-->
+                                            </section>
+                                            <footer></footer>
+                                        </article>
+                                    </li>
+                                    
+                                    <?php
                                 endforeach;
                             ?>
                             </ul>
-                            <div class="more"><a href="/category/news/">More news →</a></div>
-                        </section>
-            
+                            
+                        </secion>
+                        
+                        
                     </div> <!-- end #main -->
     
                     <?php get_sidebar(); ?>
